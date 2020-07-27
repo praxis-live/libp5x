e9e1b1f147978978497258b0ef05fd46e269248c
Neil C Smith <neilcsmith.net@googlemail.com>
2020-07-27 17:36:58 +0100

    Comment out references to ThinkDifferent class. Need to think differently!

--- processing-core/src/main/java/processing/core/PApplet.java
+++ processing-core/src/main/java/processing/core/PApplet.java
@@ -3830,15 +3830,15 @@ public class PApplet implements PConstants {
       handleMethods("dispose");
     }
 
-    if (platform == MACOSX) {
-      try {
-        final String td = "processing.core.ThinkDifferent";
-        final Class<?> thinkDifferent = getClass().getClassLoader().loadClass(td);
-        thinkDifferent.getMethod("cleanup").invoke(null);
-      } catch (Exception e) {
-        e.printStackTrace();
-      }
-    }
+//    if (platform == MACOSX) {
+//      try {
+//        final String td = "processing.core.ThinkDifferent";
+//        final Class<?> thinkDifferent = getClass().getClassLoader().loadClass(td);
+//        thinkDifferent.getMethod("cleanup").invoke(null);
+//      } catch (Exception e) {
+//        e.printStackTrace();
+//      }
+//    }
 
   }
 
@@ -10854,18 +10854,18 @@ public class PApplet implements PConstants {
       }
     }
 
-    if (platform == MACOSX) {
-      try {
-        final String td = "processing.core.ThinkDifferent";
-        Class<?> thinkDifferent =
-          Thread.currentThread().getContextClassLoader().loadClass(td);
-        Method method =
-          thinkDifferent.getMethod("init", new Class[] { PApplet.class });
-        method.invoke(null, new Object[] { sketch });
-      } catch (Exception e) {
-        e.printStackTrace();  // That's unfortunate
-      }
-    }
+//    if (platform == MACOSX) {
+//      try {
+//        final String td = "processing.core.ThinkDifferent";
+//        Class<?> thinkDifferent =
+//          Thread.currentThread().getContextClassLoader().loadClass(td);
+//        Method method =
+//          thinkDifferent.getMethod("init", new Class[] { PApplet.class });
+//        method.invoke(null, new Object[] { sketch });
+//      } catch (Exception e) {
+//        e.printStackTrace();  // That's unfortunate
+//      }
+//    }
 
     // Set the suggested display that's coming from the command line
     // (and most likely, from the PDE's preference setting).
